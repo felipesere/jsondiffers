0a54215dfd2afd682939588fc9498321b5917547
Felipe Sere <felipesere@gmail.com>
2019-04-06 13:33:49 +0100

    Larger sample

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,62 @@
-use serde_json::{Value};
+use serde_json::Value;
 
 mod diff;
 
 fn main() -> std::result::Result<(), serde_json::error::Error> {
     // Some JSON input left_data as a &str. Maybe this comes from the user.
     let left_data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ]
-        }"#;
+    {
+		"Aidan Gillen": {
+			"array": ["Game of Thron\"es", "The Wire"],
+			"string": "some string",
+			"int": 2,
+			"aboolean": true,
+			"boolean": true,
+			"object": {
+				"foo": "bar",
+				"object1": { "new prop1": "new prop value" },
+				"object2": { "new prop1": "new prop value" },
+				"object3": { "new prop1": "new prop value" },
+				"object4": { "new prop1": "new prop value" }
+			}
+		},
+		"Amy Ryan": { "one": "In Treatment", "two": "The Wire" },
+		"Annie Fitzgerald": ["Big Love", "True Blood"],
+		"Anwan Glover": ["Treme", "The Wire"],
+		"Alexander Skarsgard": ["Generation Kill", "True Blood"],
+		"Clarke Peters": null
+	}
+        "#;
+
+    let right_data = r#"
+    {
+		"Aidan Gillen": {
+			"array": ["Game of Thrones", "The Wire"],
+			"string": "some string",
+			"int": "2",
+			"otherint": 4,
+			"aboolean": "true",
+			"boolean": false,
+			"object": {
+				"foo": "bar"
+			}
+		},
+		"Amy Ryan": ["In Treatment", "The Wire"],
+		"Annie Fitzgerald": ["True Blood", "Big Love", "The Sopranos", "Oz"],
+		"Anwan Glover": ["Treme", "The Wire"],
+		"Alexander Skarsg?rd": ["Generation Kill", "True Blood"],
+		"Alice Farmer": ["The Corner", "Oz", "The Wire"]
+	}
+    "#;
 
     // Parse the string of left_data into serde_json::Value.
     let left_value: Value = serde_json::from_str(left_data)?;
-    let right_value: Value = serde_json::from_str(left_data)?;
+    let right_value: Value = serde_json::from_str(right_data)?;
 
-    println!("{:#?}", left_value);
-    println!("Please call {} at the number {}", left_value["name"], left_value["phones"][0]);
 
-    crate::diff::calculate(left_value, right_value);
+    let diffrences = crate::diff::calculate(left_value, right_value);
+
+    println!("{:#?}", diffrences);
 
     // Access parts of the left_data by indexing with square brackets.
 
